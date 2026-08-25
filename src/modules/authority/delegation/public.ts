48b2f4999a8a99d95db993ff9b90d566803d2dd6
Joel Chan <joelcsychan@gmail.com>
2026-08-26 01:33:32 +0800

    feat(authority): enforce delegation trust chains

---
+++ src/modules/authority/delegation/public.ts
@@ -0,0 +1,28 @@
+export {
+  DelegationError,
+  DelegationService,
+  delegationGrantRef,
+  delegationSnapshotRef,
+  generateDelegationGrantRef,
+  generateDelegationSnapshotRef,
+} from './delegation'
+
+export type {
+  AdmitConsequenceRequest,
+  DelegateGrantRequest,
+  DelegationAuthorityAncestor,
+  DelegationAuthoritySnapshot,
+  DelegationCommit,
+  DelegationContextPort,
+  DelegationErrorCode,
+  DelegationGrant,
+  DelegationGrantLifecycle,
+  DelegationGrantRef,
+  DelegationGrantReplacement,
+  DelegationServiceOptions,
+  DelegationSnapshotRef,
+  DelegationStore,
+  DelegationTransaction,
+  IssueRootGrantRequest,
+  RevokeGrantRequest,
+} from './delegation'
